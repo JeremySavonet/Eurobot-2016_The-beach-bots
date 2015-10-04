e545062a0cfa812b655cdb980236bf4494120fbf
savonetj <savonetj@MacBook-Pro-de-savonetj.local>
2015-10-04 17:18:39 +0200

    Fix issue with lwip and fatfs Update HelloWolrd project

--- sw/HelloWorld/main.c
+++ sw/HelloWorld/main.c
@@ -1,19 +1,42 @@
 /*
-* Simple demo program
-*
-* Description: A simple helloworld program that blink a led
-* Author: JS
-* Date: 2015-10-03
-* Version: V0.1
-*/
+ * Simple demo program
+ *
+ * Description: 
+ * A simple HelloWorld program that create a task that blinks a led
+ *
+ * Author: JS
+ * Date: 2015-10-03
+ * Version: V0.1
+ */
+
+#include <stdio.h>
+#include <string.h>
 
 #include "ch.h"
 #include "hal.h"
 #include "test.h"
 
+/*===========================================================================*/
+/* Main and generic code.                                                    */
+/*===========================================================================*/
+
+/*
+ * Green LED blinker thread, times are in milliseconds.
+ */
+static THD_WORKING_AREA( waThread1, 128 );
+static THD_FUNCTION( Thread1, arg )
+{
+    (void)arg;
+    chRegSetThreadName( "blinker" );
+    while( true )
+    {
+        palTogglePad( GPIOC, GPIOC_LED );
+        chThdSleepMilliseconds( 500 );
+    }
+}
+
 int main( void )
 {
-
   /*
    * System initializations.
    * - HAL initialization, this also initializes the configured device drivers
@@ -21,13 +44,24 @@ int main( void )
    * - Kernel initialization, the main() function becomes a thread and the
    *   RTOS is active.
    */
-  halInit();
-  chSysInit();
+    halInit();
+    chSysInit();
 
-  while( true )
-  {
-    chThdSleepMilliseconds( 500 );
-  }
+  /*
+   * Creates the blinker thread.
+   */
+    chThdCreateStatic( waThread1,
+                       sizeof( waThread1 ),
+                       NORMALPRIO,
+                       Thread1,
+                       NULL );
 
-  return 0;
+  /*
+   * Normal main() thread activity, in this demo it does nothing except
+   * sleeping in a loop and listen for events.
+   */
+    while( true )
+    {
+        // DO NOTHING
+    }
 }
