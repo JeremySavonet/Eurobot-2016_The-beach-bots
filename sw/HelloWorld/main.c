1cdac30b1e0727f10d69523a43638bbd66735c76
savonetj <jeremy.savonet@gmail.com>
2015-10-24 16:43:22 +0200

    rework the way we finish thread processing

--- sw/HelloWorld/main.c
+++ sw/HelloWorld/main.c
@@ -24,8 +24,9 @@
 /* Main and generic code.                                                    */
 /*===========================================================================*/
 
+static bool running = false;
 static int gameTick = 0;
-static thread_t *tp;
+static thread_t *tp = NULL;
 static virtual_timer_t gameTimer;
 
 /*
@@ -38,14 +39,8 @@ void runGame( void *p )
     chSysLockFromISR();
     chVTSetI( &gameTimer, MS2ST( 1000 ), runGame, p );
     chSysUnlockFromISR();
- 
-    
-    // Wait 90s and stop all the thread properly
-    if( ++gameTick >= 90 )
-    {
-        // Stop the blinker thread
-        chThdWait( tp ); 
-    }
+
+    if( ++gameTick < 90 ) {}
 }
 
 /*
@@ -69,10 +64,35 @@ static THD_FUNCTION( Thread1, arg )
     chRegSetThreadName( "blinker" );
     while( true )
     {
+        const char data[] = "Running..."; 
+        sdWrite( &SD3, (uint8_t *) data, strlen( data ) ); 
+ 
         palTogglePad( GPIOC, GPIOC_LED );
         chThdSleepMilliseconds( 500 );
+
+        // Wait 90s and stop all the thread properly
+        if( gameTick >= 5 )
+        {
+            msg_t msg;
+            chThdExit( msg );
+        }
     }
 }
+
+static THD_WORKING_AREA( waThread2, 128 );
+static THD_FUNCTION( Thread2, arg )
+{
+    (void)arg;
+    chRegSetThreadName( "killer" );
+   
+    // Wait here all thread to terminate properly 
+    chThdWait( tp );
+    
+    const char data[] = "Stop..."; 
+    sdWrite( &SD3, (uint8_t *) data, strlen( data ) );
+    palSetPad( GPIOC, GPIOC_LED );   
+}
+
 
 int main( void )
 {
@@ -86,38 +106,54 @@ int main( void )
     halInit();
     chSysInit();
 
-    // Start game timer
-    chVTSet( &gameTimer, MS2ST( 1000 ), runGame, NULL );
- 
     palSetPadMode( GPIOC, GPIOC_LED, PAL_MODE_OUTPUT_PUSHPULL );
 
-    // used function : USART3_TX 
+    // Used function : USART3_TX 
     palSetPadMode( GPIOB, 10, PAL_MODE_ALTERNATE( 7 ) ); 
     
-    // used function : USART3_RX 
+    // Used function : USART3_RX 
     palSetPadMode( GPIOB, 11, PAL_MODE_ALTERNATE( 7 ) ); 
     
-    // starts the serial driver with uartCfg as a config 
+    // Starts the serial driver with uartCfg as a config 
     sdStart( &SD3, &uartCfg ); 
-    const char data[] = "Hello World ! \n \r"; 
- 
-    /*
-     * Creates the blinker thread.
-     */
-    tp = chThdCreateStatic( waThread1,
-                           sizeof( waThread1 ),
-                           NORMALPRIO,
-                           Thread1,
-                           NULL );
-
+    
+    const char data[] = "Ready..."; 
+    
+    sdWrite( &SD3, (uint8_t *) data, strlen( data ) );  
+    
+    // Board is ready
+    palClearPad( GPIOC, GPIOC_LED ); 
+    
     /*
      * Normal main() thread activity, in this demo it does nothing except
      * sleeping in a loop and listen for events.
      */
     while( true )
     {
-        // Writes "Hello World in the UART output
-        sdWrite( &SD3, (uint8_t *) data, strlen( data ) ); 
-        chThdSleepMilliseconds( 500 ); /* Iddle thread */
+        // Wait start button
+        if ( palReadPad( GPIOA, GPIOA_BUTTON_WKUP ) != 0 && running == false ) 
+        {
+            // Start game timer
+            chVTSet( &gameTimer, MS2ST( 1000 ), runGame, NULL );
+            
+            /*
+             * Creates the blinker thread.
+             */ 
+            tp = chThdCreateStatic( waThread1,
+                                    sizeof( waThread1 ),
+                                    NORMALPRIO,
+                                    Thread1,
+                                    NULL );
+           
+            chThdCreateStatic( waThread2,
+                               sizeof( waThread2 ),
+                               NORMALPRIO,
+                               Thread2,
+                               NULL );
+            
+            running = true;
+        }
+         
+        chThdSleepMilliseconds( 100 ); /* Iddle thread */
     }
 }
